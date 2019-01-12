e699efa0274a386224fe7efb17ada72f023bb8f2
zmj <zhouminjun@kezaihui.com>
2019-01-12 17:08:08 +0800

    0020.valid_parentheses(python)

---
+++ 0020.valid_parentheses/solution.py
@@ -0,0 +1,13 @@
+class Solution:
+
+    def is_valid(self, s):
+        mapping = {')': '(', '}': '{', ']': '['}
+        stack = []
+        for char in s:
+            if char in '([{':
+                stack.append(char)
+            elif stack and stack[-1] == mapping[char]:
+                stack.pop()
+            else:
+                return False
+        return not stack
