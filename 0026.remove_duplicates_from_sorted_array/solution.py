8faa51030bec7109ecc797ca7c26e4eebece74ca
zhouminjun <zhouminjun@kezaihui.com>
2019-01-13 16:03:01 +0800

    0026.remove_duplicates_from_sorted_array(python)

---
+++ 0026.remove_duplicates_from_sorted_array/solution.py
@@ -0,0 +1,11 @@
+class Solution:
+
+    def remove_duplicates(self, nums):
+        total = 0
+        if not nums:
+            return total
+        for index, num in enumerate(nums[:]):
+            if index != 0 and num != nums[total]:
+                total += 1
+                nums[total] = nums[index]
+        return total + 1
