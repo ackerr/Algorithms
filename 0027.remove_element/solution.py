2a744a912511ba853c9586dc412fa5e3e8633d2e
zmj <zhouminjun@kezaihui.com>
2019-01-14 23:58:01 +0800

    0027.remove_element(python)

---
+++ 0027.remove_element/solution.py
@@ -0,0 +1,9 @@
+class Solution:
+
+    def remove_element(self, nums: list, val: int) -> int:
+        flag = 0
+        for index in range(len(nums)):
+            if nums[index] != val:
+                nums[flag] = nums[index]
+                flag += 1
+        return flag
