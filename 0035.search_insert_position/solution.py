120490fe80e77695bd1bf5d6006041ec745d2c00
zmj <zhouminjun@kezaihui.com>
2019-01-16 22:18:52 +0800

    0035.search_insert_position(python)

---
+++ 0035.search_insert_position/solution.py
@@ -0,0 +1,7 @@
+class Solution:
+
+    def search_insert(self, nums, target):
+        for index, num in enumerate(nums):
+            if target <= num:
+                return index
+        return len(nums)
