00038763559bc7c8792d239a5724f79bbc200f61
zmj <zhouminjun@kezaihui.com>
2019-01-20 23:20:42 +0800

    0038.count_and_say(python)

---
+++ 0038.count_and_say/solution.py
@@ -0,0 +1,14 @@
+import re
+
+
+class Solution:
+
+    def count_and_say(self, n):
+        data = '1'
+        for _ in range(n-1):
+            data = re.sub(r'(.)\1*', lambda m: str(len(m.group(0)) + m.group(1)), n)
+        return data
+
+
+if __name__ == '__main__':
+    print(re.findall(r'(.)\1*', 'aaaabcd'))  # ['a', 'b', 'c', 'd']
