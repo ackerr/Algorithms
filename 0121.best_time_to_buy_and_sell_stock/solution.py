a4d5dd91a8fa8b329232f2a9feb0cc2f63b173b1
zhouminjun <zhouminjun@kezaihui.com>
2019-02-13 18:07:29 +0800

    0121.Best Time to Buy and Sell Stock(python)

---
+++ 0121.best_time_to_buy_and_sell_stock/solution.py
@@ -0,0 +1,11 @@
+class Solution:
+
+    def max_profit(self, prices):
+        min_price, max_profit = 9999999, 0
+        for price in prices:
+            if min_price > price:
+                min_price = price
+            elif max_profit < price - min_price:
+                max_profit = price - min_price
+        return max_profit
+
