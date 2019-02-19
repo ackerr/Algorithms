e76e5b9501c5c7ed4c176ceb8d80e27095d3dc95
zhouminjun <zhouminjun@kezaihui.com>
2019-02-19 18:52:11 +0800

    0136.single_number(python)

---
+++ 0136.single_number/solution.py
@@ -0,0 +1,18 @@
+class Solution:
+
+    def single_number(self, nums):
+        """
+        0^1 = 1
+        1^1 = 0
+        """
+        value = 0
+        for num in nums:
+            value ^= num
+        return value
+
+    def other_solution(self, nums):
+        """
+        [1,1,2,2,3] + [3] == [1,2,3] * 2
+        """
+        return sum(set(nums)) * 2 - sum(nums)
+
