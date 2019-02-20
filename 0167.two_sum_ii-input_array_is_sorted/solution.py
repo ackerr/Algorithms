12be3692f30d38f18d305bcc3cd5933e7a769be8
zhouminjun <zhouminjun@kezaihui.com>
2019-02-20 19:05:26 +0800

    0167.two_sum_II-input_array_is_sorted(python)

---
+++ 0167.two_sum_ii-input_array_is_sorted/solution.py
@@ -0,0 +1,19 @@
+class Solution:
+
+    def two_sum(self, numbers,  target):
+        flag = {}
+        for index, number in enumerate(numbers):
+            sub = target - number
+            if sub in flag.keys():
+                return flag[sub] + 1, index + 1
+            else:
+                flag[number] = index
+
+    def loop_solution(self, numbers, target):
+        left, right = 0, len(numbers) - 1
+        while numbers[left] + numbers[right] != target:
+            if numbers[left] + numbers[right] < target:
+                left += 1
+            else:
+                right -= 1
+        return left + 1, right + 1
