3333911170051a6ea55258355e7fd7da96fa86fd
ZmJ <1352252181@qq.com>
2019-02-27 23:44:02 +0800

    0021.merge_two_sorted_lists(python)

---
+++ 0021.merge_two_sorted_lists/README.md
@@ -0,0 +1,19 @@
+Merge two sorted linked lists and return it as a new list. The new list should be made by splicing together the nodes of the first two lists.
+
+Example:
+```
+Input: 1->2->4, 1->3->4
+Output: 1->1->2->3->4->4
+```
+
+---
+
+译：
+
+将两个有序链表合并为一个新的有序链表并返回。新链表是通过拼接给定的两个链表的所有节点组成的。 
+
+示例：
+```
+输入：1->2->4, 1->3->4
+输出：1->1->2->3->4->4
+```
---
+++ 0021.merge_two_sorted_lists/solution.py
@@ -0,0 +1,22 @@
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+
+class Solution:
+    """ 用上方的类模拟链表 """
+
+    def merge_two_lists(self, l1: ListNode, l2: ListNode) -> ListNode:
+        cur = ans = ListNode(0)  # 定义两个空链表，一个用于过程，一个用于结尾
+        while l1 and l2:
+            if l1.val > l2.val:  # 接上小的值
+                cur.next = l2
+                l2 = l2.next
+            else:
+                cur.next = l1
+                l1 = l1.next
+            cur = cur.next
+        cur.next = l1 or l2  # 填补多出的值
+        return ans.next  # 去掉初始化的0
