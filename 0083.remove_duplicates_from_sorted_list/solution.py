dec0353bc6c82e544cc231c25758be3a62739110
ZmJ <1352252181@qq.com>
2019-02-28 23:30:07 +0800

    0083.remove_duplicates_from_sorted_list(python)

---
+++ 0083.remove_duplicates_from_sorted_list/solution.py
@@ -0,0 +1,17 @@
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+
+class Solution:
+
+    def delete_duplicates(self, head: ListNode) -> ListNode:
+        cur = head
+        while cur and cur.next:
+            if cur.val == cur.next.val:
+                cur.next = cur.next.next
+            else:
+                cur = cur.next
+        return head
