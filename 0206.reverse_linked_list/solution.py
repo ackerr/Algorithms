f23a2f93819b6d7c40e7aaadaf315cd8169ad6fe
zhouminjun <zhouminjun@kezaihui.com>
2019-03-03 16:25:16 +0800

    0206.reverse_linked_list(python)

---
+++ 0206.reverse_linked_list/solution.py
@@ -0,0 +1,26 @@
+class ListNode:
+
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+
+class Solution:
+
+    def reverse_list(self, head: ListNode) -> ListNode:
+        cur, ans = head, None
+        while cur:
+            temp = cur.next
+            cur.next = ans
+            ans = cur
+            cur = temp
+        return ans
+
+    def recursion_solution(self, head: ListNode, ans=None) -> ListNode:
+        if not head:
+            return ans
+        temp = head.next
+        head.next = ans
+        ans = head
+        head = temp
+        return self.recursion_solution(head, ans)
