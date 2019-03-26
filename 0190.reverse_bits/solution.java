efaf3e8f46ce1cd2a58e03c37930559b13eef201
ZmJ <1352252181@qq.com>
2019-03-26 23:11:46 +0800

    0190.reverse_bits(python, java)

---
+++ 0190.reverse_bits/solution.java
@@ -0,0 +1,12 @@
+class solution{
+
+    public int reverseBits(int n){
+        int ans = 0;
+        for (int i=0;i < 32;i++){
+            ans = (ans << 1) + (n & 1);
+            n >>= 1;
+        }
+        return ans;
+    }
+
+}
---
+++ 0190.reverse_bits/solution.py
@@ -0,0 +1,17 @@
+class Solution:
+
+    def reverse_bits(self, n):
+        """
+        2 << 1 == 2 * 2
+        2 >> 1 == 2 / 2
+        1 & 1 == 1
+        1 & 0 == 0
+        0 & 0 == 0
+
+        """
+        ans = 0
+        for i in range(32):
+            ans = (ans << 1) + (n & 1)
+            n >>= 1
+        return ans
+
