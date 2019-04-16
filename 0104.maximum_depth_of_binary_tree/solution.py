9ce11040f7961cf1955cc20b650013097cfa7f40
ZmJ <1352252181@qq.com>
2019-04-16 22:35:44 +0800

    0104/0107 (python, java)

---
+++ 0104.maximum_depth_of_binary_tree/solution.py
@@ -0,0 +1,11 @@
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+class Solution:
+    def max_depth(self, root: TreeNode)
+        if not root:
+            return 0
+        return max([self.max_depth(root.left), self.max_depth(root.right)]) + 1
---
+++ 0107.binary_tree_level_order_traversal_II/solution.py
@@ -0,0 +1,24 @@
+from typing import List
+
+
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+
+class Solution:
+    def level_order_bottom(self, root: TreeNode) -> List[List[int]]:
+        """ use stack """
+        stack = [(root, 0)]
+        ans = []
+        while stack:
+            node, level = stack.pop()
+            if node:
+                if len(ans) < level+1:
+                    ans.append([])
+                ans[level].append(node.val)
+                stack.append((node.right, level+1)
+                stack.append((node.left, level+1)
+        return ans[::-1]
