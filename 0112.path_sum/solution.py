ccb1d6e804c0ab42fc15836b8bc3354bb9fde5b4
ZmJ <1352252181@qq.com>
2019-04-24 09:12:35 +0800

    0122.path_sum(pythonm java)

---
+++ 0112.path_sum/solution.py
@@ -0,0 +1,13 @@
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+class Solution:
+    def hasPathSum(self, root: TreeNode, sum: int) -> bool:
+        if not root:
+            return False
+        if root.val == sum and not root.left and not root.right:
+            return True
+        return self.hasPathSum(root.left, sum - root.val) or self.hasPathSum(root.right, sum - root.val)
