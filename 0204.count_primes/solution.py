6a1ccbdb45d5433d575268820e3e877b9e31e38d
ZmJ <1352252181@qq.com>
2019-05-11 23:15:27 +0800

    0204.count_prime(python, java)

---
+++ 0204.count_primes/solution.py
@@ -0,0 +1,10 @@
+class Solution:
+    def countPrimes(self, n: int) -> int:
+        if n < 3:
+            return 0
+        primes = [1] * n
+        primes[0] = primes[1] = 0
+        for i in range(2, int(n ** 0.5) + 1):
+            if primes[i]:
+                primes[i * i: n: i] = [0] * len(primes[i * i: n: i])
+        return sum(primes)
