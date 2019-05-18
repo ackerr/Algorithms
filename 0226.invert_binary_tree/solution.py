4fb760b026471bb3570d20b338514907dc53803c
ZmJ <1352252181@qq.com>
2019-05-18 21:38:02 +0800

    0226.invert_binary_tree(python, java)

---
+++ 0226.invert_binary_tree/solution.py
@@ -0,0 +1,17 @@
+Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+class Solution:
+    def invert_tree(self, root: TreeNode) -> TreeNode:
+        """ 对调left 与 right """
+        if not root:
+            return None
+        root.left, root.right = root.right. root.left
+        self.invert_tree(root.left)
+        self.invert_tree(root.right)
+        return root
+        
