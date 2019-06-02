7a7b01c2b722379847dd07f23a9cf516c6becfbc
ZmJ <1352252181@qq.com>
2019-06-02 16:26:11 +0800

    0429(python, java)

---
+++ 0429.N-ary_tree_level_order_traversal/README.md
@@ -0,0 +1,16 @@
+Given an n-ary tree, return the level order traversal of its nodes' values. (ie, from left to right, level by level).
+
+
+![](https://assets.leetcode.com/uploads/2018/10/12/narytreeexample.png)
+
+For example, given a 3-ary tree:
+
+```
+We should return its level order traversal:
+
+[
+     [1],
+     [3,2,4],
+     [5,6]
+]
+```
---
+++ 0429.N-ary_tree_level_order_traversal/solution.py
@@ -0,0 +1,11 @@
+class Solution:
+    def level_order(self, root: 'Node') -> List[List[int]]:
+        ans, nodes = [], [root]
+        while nodes:
+            temp, temp_nodes = [], []
+            for node in nodes:
+                temp.append(node.val)
+                temp_nodes.extend(node.children)
+            ans.append(temp)
+            nodes = temp_nodes
+        return ans
