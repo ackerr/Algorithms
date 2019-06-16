d58ac7305c85f56b04ed0a426ab422984ecaa0a4
ZmJ <1352252181@qq.com>
2019-06-16 23:26:23 +0800

    0278.fir_bad_version(python)

---
+++ 0278.first_bad_version/solution.py
@@ -0,0 +1,15 @@
+# The isBadVersion API is already defined for you.
+# @param version, an integer
+# @return a bool
+# def isBadVersion(version):
+
+class Solution:
+    def firstBadVersion(self, n):
+        l, r = 0, n
+        while l <= r:
+            mid = (l + r) // 2
+            if not isBadVersion(mid):
+                l = mid + 1
+            else:
+                r = mid - 1
+        return l
