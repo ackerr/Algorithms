29a41dd222c6d08d6c6e39c9c028cbd55358e3b8
ZmJ <1352252181@qq.com>
2019-06-17 23:11:26 +0800

    0283.move_zeroes(python)

---
+++ 0283.move_zeroes/solution.py
@@ -0,0 +1,12 @@
+class Solution:
+    def move_zeroes(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        last = 0
+        for i in range(len(nums)):
+            if nums[i] != 0:
+                nums[i], nums[last] = nums[last], nums[i]
+                last += 1
+
+        
