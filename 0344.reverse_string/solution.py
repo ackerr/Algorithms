928b995ed46adef86ba6094fedc5d4e0eb9e4075
wzmmmmj <wzmmmmj@gmail.com>
2019-07-01 22:27:01 +0800

    0344.reverse_string(python)

---
+++ 0344.reverse_string/solution.py
@@ -0,0 +1,10 @@
+from typing import List
+
+
+class Solution:
+    def reverse_string(self, s: List[str]) -> None:
+        """
+        Do not return anything, modify s in-place instead.
+        """
+        for i in range(len(s)//2):
+            s[i], s[-i-1] = s[-i-1], s[i]
