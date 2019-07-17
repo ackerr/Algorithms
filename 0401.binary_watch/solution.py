6f9e73648d19ab8522938a52ead2081d9bda10c3
wzmmmmj <wzmmmmj@gmail.com>
2019-07-17 23:16:46 +0800

    0401(python)

---
+++ 0401.binary_watch/solution.py
@@ -0,0 +1,11 @@
+from typing import List
+
+
+class Solution:
+    def read_binary_watch(self, num: int) -> List[str]:
+        ans = []
+        for i in range(12):
+            for j in range(60):
+                if bin(i).count('1') + bin(j).count('1') == num:
+                    ans.append('%d:%02d' % (i, j))
+        return ans
