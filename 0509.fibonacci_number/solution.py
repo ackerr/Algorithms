11c3ba1497aaa7adee4a8c2008a2e8b612f8bd9c
ZmJ <wzmmmmj@gmail.com>
2019-10-06 17:52:27 +0800

    0509(python)

---
+++ 0509.fibonacci_number/solution.py
@@ -0,0 +1,16 @@
+class Solution:
+    def fib_loop(self, n: int) -> int:
+        a, b = 0, 1
+        for _ in range(n):
+            a, b = b, a + b
+        return a
+
+    def fib_recursion(self, n: int) -> int:
+        if n < 2:
+            return n
+        return self.fib_recursion(n - 1) + self.fib_recursion(n - 2)
+
+
+if __name__ == '__main__':
+    assert Solution().fib_loop(10) == 55
+    assert Solution().fib_recursion(10) == 55
