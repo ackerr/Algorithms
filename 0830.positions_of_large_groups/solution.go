c8b6ba6d5d41a9120484497ea455d9860eafd965
ZmJ <wzmmmmj@gmail.com>
2019-10-07 18:39:41 +0800

    0830(go)

---
+++ 0830.positions_of_large_groups/solution.go
@@ -0,0 +1,15 @@
+package main
+
+func largeGroupPositions(S string) [][]int {
+	var ans [][]int
+	var i int
+	for j := 0; j < len(S); j++ {
+		if j == len(S)-1 || S[j] != S[j+1] {
+			if j-i > 1 {
+				ans = append(ans, []int{i, j})
+			}
+			i = j + 1
+		}
+	}
+	return ans
+}
