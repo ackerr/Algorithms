49fea18fee7130bebcf067315b2730f7d69de047
ZmJ <wzmmmmj@gmail.com>
2019-10-11 23:37:26 +0800

    0011(go)

---
+++ 0011.container_with_most_water/solution.go
@@ -0,0 +1,36 @@
+package main
+
+import "fmt"
+
+func Min(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
+}
+
+func Max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
+
+func maxArea(height []int) int {
+	maxArea := 0
+	var left, right = 0, len(height)-1
+	for ; left < right; {
+		area := Min(height[left], height[right]) * (right - left)
+		maxArea = Max(maxArea, area)
+		if height[left] < height[right] {
+			left++
+		} else {
+			right--
+		}
+	}
+	return maxArea
+}
+
+func main() {
+	fmt.Println(maxArea([]int{1, 1, 1, 5, 5, 1}))
+}
