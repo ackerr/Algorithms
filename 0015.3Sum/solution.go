6e43efd350e110f88dafada581cf3c604c42dd67
ZmJ <wzmmmmj@gmail.com>
2019-10-16 23:27:00 +0800

    0015(go)

---
+++ 0015.3Sum/solution.go
@@ -0,0 +1,38 @@
+package main
+
+import "sort"
+
+func three_sum(nums []int) [][]int {
+	var ans [][]int
+	sort.Ints(nums)
+
+	for i := 0; i < len(nums)-2; i++ {
+		l, r := i+1, len(nums)-1
+		if i > 0 && nums[i] == nums[i-1] { // distinct
+			continue
+		}
+		for l < r {
+
+			if l > i+1 && nums[l] == nums[l-1] { // distinct
+				l++
+				continue
+			}
+			if r < len(nums)-2 && nums[r] == nums[r+1] { // distinct
+				r--
+				continue
+			}
+			sum := nums[i] + nums[l] + nums[r]
+
+			if sum < 0 {
+				l++
+			} else if sum > 0 {
+				r--
+			} else {
+				ans = append(ans, []int{nums[i], nums[l], nums[r]})
+				l++
+				r--
+			}
+		}
+	}
+	return ans
+}
