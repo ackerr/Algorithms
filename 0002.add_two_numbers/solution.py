a1c69d9190d6fe21f169ad271d1ecbc117cbc621
ZmJ <wzmmmmj@gmail.com>
2019-10-19 22:43:52 +0800

    0002(python)

---
+++ 0002.add_two_numbers/solution.py
@@ -0,0 +1,21 @@
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+
+class Solution:
+    def add_two_numbers(self, l1: ListNode, l2: ListNode) -> ListNode:
+        res = ans = ListNode(0)
+        pon = 0
+
+        while pon or l1 or l2:
+            i = (l1.val if l1 else 0) + (l2.val if l2 else 0) + pon
+            pon = i // 10
+            ans.next = ListNode(i % 10)
+            ans = ans.next
+            l1 = l1.next if l1 else None
+            l2 = l2.next if l2 else None
+
+        return res.next
