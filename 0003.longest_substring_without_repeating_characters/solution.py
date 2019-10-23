0f15013b590fc1b55d22127ccb1d7430bd8f16dc
ZmJ <wzmmmmj@gmail.com>
2019-10-23 23:13:04 +0800

    0003(python)

---
+++ 0003.longest_substring_without_repeating_characters/solution.py
@@ -0,0 +1,14 @@
+class Solution:
+    def length_of_longest_substring(self, s: str) -> int:
+        count = res = 0
+        sign = {}
+        for i in range(len(s)):
+            if s[i] in sign:  # duplicate
+                count = max(sign[s[i]], count)
+            sign[s[i]] = i + 1  # update index
+            res = max(res, i - count + 1)  # update max value
+        return res
+
+
+if __name__ == '__main__':
+    assert Solution().length_of_longest_substring("abba") == 2
