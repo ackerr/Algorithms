bf8378b4d867c7846f71627d70515fae515109b2
ZmJ <wzmmmmj@gmail.com>
2019-12-02 23:24:20 +0800

    0082(python)

---
+++ 0082.remove_duplicates_from_sorted_list_II/solution.py
@@ -0,0 +1,22 @@
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+
+class Solution:
+    def delete_duplicates(self, head: ListNode) -> ListNode:
+        if not head or not head.next:
+            return head
+        node = ListNode(None)
+        node.next = head
+        target = node
+        while target.next:
+            left = right = target.next
+            while right.next and right.next.val == left.val:  # duplicate
+                right = right.next
+            if left == right:  # no move
+                target = target.next
+            else:
+                target.next = right.next  # remove duplicate node
+        return node.next
