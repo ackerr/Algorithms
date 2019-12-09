3e48cfd9eb07fcf64168e05ac3089b01d23a6bf1
ZmJ <wzmmmmj@gmail.com>
2019-12-09 23:40:50 +0800

    0033（python)

---
+++ 0033.search_in_rotated_sorted_array/solution.py
@@ -0,0 +1,26 @@
+from typing import List
+
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        left, right = 0, len(nums) - 1
+        while left <= right:
+            mid = left + ((right - left) >> 1)
+            if nums[mid] == target:
+                return mid
+            if nums[left] <= nums[mid]:
+                if nums[left] <= target < nums[mid]:
+                    right = mid - 1
+                else:
+                    left = mid + 1
+            elif nums[mid] < nums[right]:
+                if nums[mid] < target <= nums[right]:
+                    left = mid + 1
+                else:
+                    right = mid - 1
+        return -1
+
+
+if __name__ == '__main__':
+    nums = [14,25,36,37,4,9,10,13]
+    assert Solution().search(nums, 36) == 2
+
