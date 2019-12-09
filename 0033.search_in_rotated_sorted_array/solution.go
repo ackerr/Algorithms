f893ba7338cb13f24bc229d03a0f549d5cb224cd
ZmJ <wzmmmmj@gmail.com>
2019-12-09 23:55:35 +0800

    0033(go)

---
+++ 0033.search_in_rotated_sorted_array/solution.go
@@ -0,0 +1,27 @@
+package main
+
+
+func search(nums []int, target int) int {
+	left, right := 0, len(nums)-1
+	for left <= right {
+		mid := left + ((right - left) >> 1)
+		if nums[mid] == target {
+			return mid
+		}
+		if nums[left] <= nums[mid] {
+			if nums[left] <= target && target < nums[mid] {
+				right = mid - 1
+			} else {
+				left = mid + 1
+			}
+		} else if nums[mid] < nums[right] {
+			if nums[mid] < target && target <= nums[right] {
+				left = mid + 1
+
+			} else {
+				right = mid - 1
+			}
+		}
+	}
+	return -1
+}
---
+++ 0033.search_in_rotated_sorted_array/solution_test.go
@@ -0,0 +1,22 @@
+package main
+
+import "testing"
+
+
+func TestSearch(t *testing.T) {
+	nums := []int{6, 7, 8, 9, 1, 3, 4, 5}
+	target := 8
+	index := search(nums, target)
+	if index != 2{
+		t.Errorf("target %d index error", target)
+	}
+
+	nums = []int{16, 27, 38, 49, 1, 3, 4, 5, 10, 13}
+	target = 8
+	index = search(nums, target)
+	if index != -1 {
+		t.Errorf("target %d index error", target)
+	}
+}
+
+
