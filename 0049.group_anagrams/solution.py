9f8ddf7cb29c6ce614ad5fe8fbc7a254ce5e1c27
ZmJ <wzmmmmj@gmail.com>
2019-12-16 23:32:07 +0800

    0049(python)

---
+++ 0049.group_anagrams/solution.py
@@ -0,0 +1,17 @@
+from typing import List
+
+
+class Solution:
+    def group_anagrams(self, strs: List[str]) -> List[List[str]]:
+        ans = {}
+        for i in strs:
+            j = str(sorted(i))
+            if j not in ans.keys():
+                ans[j] = [i]
+            else:
+                ans[j].append(i)
+        return [i for i in ans.values()]
+
+
+if __name__ == '__main__':
+    assert Solution().group_anagrams(["abc", "cba", "baa", "aba"]) == [["abc", "cba"], ["baa", "aba"]]
