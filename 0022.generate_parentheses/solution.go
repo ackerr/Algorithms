e5d5d78c545522e083b85d4d5e1a170d4d7238cb
ZmJ <wzmmmmj@gmail.com>
2019-12-25 00:22:07 +0800

    0022(go)

---
+++ 0022.generate_parentheses/solution.go
@@ -0,0 +1,28 @@
+package main
+
+import "fmt"
+
+func generateParenthesis(n int) []string {
+	ans := make([]string, 0)
+	backtrack("", n, n, &ans)
+	return ans
+}
+
+func backtrack(s string, left, right int, ans *[]string) {
+	fmt.Println(left, right)
+	if left == 0 {  // s中没左括号了，其余的都是不匹配的值
+		for i:=0;i<right;i++{
+			s += ")"
+		}
+		*ans = append(*ans, s)
+		return
+	}
+	backtrack(s+"(", left-1, right, ans)
+	if left < right {
+		backtrack(s+")", left, right-1, ans)
+	}
+}
+
+func main() {
+	generateParenthesis(3)
+}
---
+++ 0022.generate_parentheses/solution_test.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"testing"
+)
+
+func TestGenerateParentheses(t *testing.T){
+	ans := generateParenthesis(2)
+	if len(ans) != 2{
+		t.Errorf("resutls error")
+	}
+
+	ans = generateParenthesis(3)
+	if len(ans) != 5{
+		t.Errorf("resutls error")
+	}
+}
