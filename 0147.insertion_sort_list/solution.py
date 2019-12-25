17f38f850b0dc00fa9e440d46a447c277ee0841f
ZmJ <wzmmmmj@gmail.com>
2019-12-25 23:48:48 +0800

    0147(python)

---
+++ 0147.insertion_sort_list/solution.py
@@ -0,0 +1,25 @@
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+
+class Solution:
+    def insertion_sort_list(self, head: ListNode) -> ListNode:
+        """ 因为单向链表，不能像数组一样从后向前比较，而是从前往后直接找到对于的位置插入 """
+        if not head or not head.next:  # empty or one element
+            return head
+        ans = ListNode(0)
+        ans.next = head
+        while head and head.next:
+            if head.val <= head.next.val:  # greater than prev
+                head = head.next
+                continue
+            pre = ans
+            while pre.next.val < head.next.val:  # insert index
+                pre = pre.next
+            temp = head.next  # move element
+            head.next = temp.next
+            temp.next = pre.next
+            pre.next = temp
+        return ans.next
