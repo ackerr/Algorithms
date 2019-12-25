533da5c601bb5a5e8b99afd3531b302f7a5c1e83
ZmJ <wzmmmmj@gmail.com>
2019-12-26 00:44:59 +0800

    0147(go)

---
+++ 0147.insertion_sort_list/solution.go
@@ -0,0 +1,30 @@
+package main
+
+type ListNode struct {
+	Val int
+	Next *ListNode
+}
+
+
+func insertionSortList(head *ListNode) *ListNode {
+	if head == nil || head.Next == nil{
+		return head
+	}
+	ans := ListNode{Val: 0}
+	ans.Next = head
+	for head != nil &&  head.Next != nil{
+		if head.Val <= head.Next.Val{
+			head = head.Next
+			continue
+		}
+		pre := &ans
+		for pre.Next.Val < head.Next.Val{
+			pre = pre.Next
+		}
+		temp := head.Next
+		head.Next = temp.Next
+		temp.Next = pre.Next
+		pre.Next = temp
+	}
+	return ans.Next
+}
---
+++ 0147.insertion_sort_list/solution_test.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"reflect"
+	"testing"
+)
+
+
+func TestInsertionSortList(t *testing.T){
+	a := ListNode{Val:3}
+	b := ListNode{Val:4, Next: &a}
+	c := ListNode{Val:5, Next: &b}
+	d := ListNode{Val:6, Next: &c}
+	sortLinkList := insertionSortList(&d)
+	result := make([]int, 0)
+	for sortLinkList != nil{
+		result = append(result, sortLinkList.Val)
+		sortLinkList = sortLinkList.Next
+	}
+	fmt.Println(result)
+	correct := []int{3,4,5,6}
+	if !reflect.DeepEqual(result, correct){
+		t.Errorf("result error, correct result should be %d", correct)
+
+	}
+
+
+}
