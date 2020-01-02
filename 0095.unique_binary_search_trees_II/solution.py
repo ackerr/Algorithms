abe336d50e10f5557651e55622786dc11a84986a
ZmJ <wzmmmmj@gmail.com>
2020-01-02 23:25:07 +0800

    0095(python,go)

---
+++ 0095.unique_binary_search_trees_II/solution.py
@@ -0,0 +1,13 @@
+from typing import List
+
+
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+
+class Solution:
+    def generate_trees(self, n: int) -> List[TreeNode]:
+        pass
