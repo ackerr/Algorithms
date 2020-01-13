c5bb5aba0c6da1b1544ddb708d3e936b1c70989f
ZmJ <wzmmmmj@gmail.com>
2020-01-13 23:42:54 +0800

    0002(go)

---
+++ 0002.add_two_numbers/solution_test.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+    "testing"
+    "reflect"
+)
+
+
+func TestAddTwoNumbers(t *testing.T){
+    // a := ListNode{Val:1}
+    // b := ListNode{Val:2, Next: &a}
+    // c := ListNode{Val:5, Next: &b}
+
+    // d := ListNode{Val:2}
+    // e := ListNode{Val:5, Next: &a}
+    // f := ListNode{Val:0, Next: &b}
+    // ans := addTwoNumbers(&c, &c)
+
+    // value := []int{}
+    // for ans != nil{
+    //     value = append(value, ans.Val)
+    //     ans = ans.Next
+    // }
+    // expext := []int{}
+    // for f != nil{
+    //     expect = append(expect, f.Val)
+    //     f = f.Next
+    // }
+
+    // if !reflect.deepEqual(value, f){
+    //     t.Errorf("result error")
+    // }
+}
