8ea5abb1741bca3840dc0654dacdac39d098ee48
ZmJ <wzmmmmj@gmail.com>
2020-01-15 00:12:32 +0800

    0002(go)

--- 0002.add_two_numbers/solution.go
+++ 0002.add_two_numbers/solution.go
@@ -1,32 +1,31 @@
 package main
 
-
 type ListNode struct {
-     Val int
-     Next *ListNode
- }
+	Val  int
+	Next *ListNode
+}
 
 func addTwoNumbers(l1 *ListNode, l2 *ListNode) *ListNode {
-    ans := &ListNode{Val:-1}
-    cur := ans
-    flag := 0
-    for l1 != nil || l2 != nil || flag == 1 {
-        val := flag
-        if l1 != nil{
-            val += l1.Val
-        }
-        if l2 != nil{
-            val += l2.Val
-        }
-        flag = val/10
-        cur.Next = &ListNode{Val:val%10}
-        cur = cur.Next
-        if l1 != nil{
-            l1 = l1.Next
-        }
-        if l2 != nil{
-            l2 = l2.Next
-        }
-    }
-    return ans.Next
+	ans := &ListNode{Val: -1}
+	cur := ans
+	flag := 0
+	for l1 != nil || l2 != nil || flag == 1 {
+		val := flag
+		if l1 != nil {
+			val += l1.Val
+		}
+		if l2 != nil {
+			val += l2.Val
+		}
+		flag = val / 10
+		cur.Next = &ListNode{Val: val % 10}
+		cur = cur.Next
+		if l1 != nil {
+			l1 = l1.Next
+		}
+		if l2 != nil {
+			l2 = l2.Next
+		}
+	}
+	return ans.Next
 }
--- 0002.add_two_numbers/solution_test.go
+++ 0002.add_two_numbers/solution_test.go
@@ -1,33 +1,32 @@
 package main
 
 import (
-    "testing"
-    "reflect"
+	"reflect"
+	"testing"
 )
 
+func TestAddTwoNumbers(t *testing.T) {
+	a := ListNode{Val: 1}
+	b := ListNode{Val: 2, Next: &a}
+	c := ListNode{Val: 5, Next: &b}
 
-func TestAddTwoNumbers(t *testing.T){
-    // a := ListNode{Val:1}
-    // b := ListNode{Val:2, Next: &a}
-    // c := ListNode{Val:5, Next: &b}
+	d := ListNode{Val: 2}
+	e := ListNode{Val: 5, Next: &a}
+	f := ListNode{Val: 0, Next: &b}
+	ans := addTwoNumbers(&c, &c)
 
-    // d := ListNode{Val:2}
-    // e := ListNode{Val:5, Next: &a}
-    // f := ListNode{Val:0, Next: &b}
-    // ans := addTwoNumbers(&c, &c)
+	actual := []int{}
+	for ans != nil {
+		actual = append(actual, ans.Val)
+		ans = ans.Next
+	}
+	expext := []int{}
+	for f != nil {
+		expect = append(expect, f.Val)
+		f = f.Next
+	}
 
-    // value := []int{}
-    // for ans != nil{
-    //     value = append(value, ans.Val)
-    //     ans = ans.Next
-    // }
-    // expext := []int{}
-    // for f != nil{
-    //     expect = append(expect, f.Val)
-    //     f = f.Next
-    // }
-
-    // if !reflect.deepEqual(value, f){
-    //     t.Errorf("result error")
-    // }
+	if !reflect.deepEqual(actual, f) {
+		t.Errorf("result error")
+	}
 }
