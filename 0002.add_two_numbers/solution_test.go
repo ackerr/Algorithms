094a2698d42bb90199209737e4ac468b47623014
ZmJ <wzmmmmj@gmail.com>
2020-01-16 14:40:16 +0800

    fix: 0002(go test)

--- 0002.add_two_numbers/solution_test.go
+++ 0002.add_two_numbers/solution_test.go
@@ -9,10 +9,6 @@ func TestAddTwoNumbers(t *testing.T) {
 	a := ListNode{Val: 1}
 	b := ListNode{Val: 2, Next: &a}
 	c := ListNode{Val: 5, Next: &b}
-
-	d := ListNode{Val: 2}
-	e := ListNode{Val: 5, Next: &a}
-	f := ListNode{Val: 0, Next: &b}
 	ans := addTwoNumbers(&c, &c)
 
 	actual := []int{}
@@ -20,13 +16,9 @@ func TestAddTwoNumbers(t *testing.T) {
 		actual = append(actual, ans.Val)
 		ans = ans.Next
 	}
-	expext := []int{}
-	for f != nil {
-		expect = append(expect, f.Val)
-		f = f.Next
-	}
+	expect := []int{0, 5, 2}
 
-	if !reflect.deepEqual(actual, f) {
+	if !reflect.DeepEqual(actual, expect) {
 		t.Errorf("result error")
 	}
 }
