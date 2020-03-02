d6d25e90b474bd424b96e5fa027bf8582a0cf836
ZmJ <wzmmmmj@gmail.com>
2020-03-02 23:18:36 +0800

    0016(go)

---
+++ 0016.3Sum_Closest/solution.go
@@ -0,0 +1,39 @@
+package main
+
+import "sort"
+
+func threeSumClosest(nums []int, target int) int {
+	sort.Ints(nums)
+	length := len(nums)
+	if length < 3 {
+		return 0
+	}
+	ans := nums[0] + nums[1] + nums[2]
+	if length == 3 {
+		return ans
+	}
+	for i := 0; i < length-2; i++ {
+		l, r := i+1, length-1
+		for l < r {
+			s := nums[i] + nums[l] + nums[r]
+			if abs(s-target) < abs(ans-target) {
+				ans = s
+			}
+			if s < target {
+				l++
+			} else if s > target {
+				r--
+			} else {
+				return target
+			}
+		}
+	}
+	return ans
+}
+
+func abs(a int) int {
+	if a < 0 {
+		return -a
+	}
+	return a
+}
---
+++ 0016.3Sum_Closest/solution_test.go
@@ -0,0 +1,11 @@
+package main
+
+import "testing"
+
+func TestThreeSumClosest(t *testing.T) {
+	req := []int{1, 4, 5, 6, 8, 2, -5, 2, 5}
+	if threeSumClosest(req, 0) != 0 {
+		t.Errorf("result should be 0")
+	}
+
+}
