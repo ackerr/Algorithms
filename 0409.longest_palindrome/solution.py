b15111eca9cc1c6b6a793d3e00000be342ecdb5e
wzmmmmj <wzmmmmj@gmail.com>
2020-03-19 23:53:25 +0800

    0409(python)

---
+++ 0409.longest_palindrome/solution.py
@@ -0,0 +1,11 @@
+import collections
+
+
+class Solution:
+    def longestPalindrome(self, s: str) -> int:
+        count = collections.Counter(s)
+        res = 0
+        for k in count:
+            res += (count[k]//2 * 2)
+        return res if res == len(s) else res + 1
+
