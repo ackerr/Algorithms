522f0cc1a8c6755243da6687a3542217b649bdea
wzmmmmj <wzmmmmj@gmail.com>
2020-03-23 23:11:17 +0800

    0034(python)

---
+++ 0034/solution.py
@@ -0,0 +1,21 @@
+from typing import List
+
+
+class Solution:
+    def searchRange(self, nums: List[int], target: int) -> List[int]:
+        left, right = 0, len(nums) - 1
+        while left <= right:
+            mid = (left + right) // 2
+            if nums[mid] < target:
+                left = mid + 1
+            elif nums[mid] > target:
+                right = mid - 1
+            else:
+                left = right = mid
+                while left > 0 and nums[left-1] == target:
+                    left -= 1
+                while right < len(nums) -1 and nums[right+1] == target:
+                    right += 1
+                return [left, right]
+        return [-1,-1]
+
