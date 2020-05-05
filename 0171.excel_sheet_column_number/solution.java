0655bddabe10f73c2c749f1f6e68abdfe4794b82
Ackerr <wzmmmmj@gmail.com>
2020-05-05 14:34:59 +0800

    remove java solution

--- 0171.excel_sheet_column_number/solution.java
+++
@@ -1,9 +0,0 @@
-class solution {
-    public int titleToNumber(String s) {
-        int ans = 0;
-        for (int i=0;i<s.length();i++) {
-            ans = ans * 26 + (int) s.charAt(i) - 64;
-        }
-        return ans;
-    }
-}
--- 0258.add_digits/solution.java
+++
@@ -1,8 +0,0 @@
-class Solution {
-    public int addDigits(int num) {
-        if (num < 10) return num;
-        num %= 9;
-        if (num == 0) return 9;
-        return num;
-    }
-}
