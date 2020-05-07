f09e1df988d19080ebc53a7f79138accede9acc1
Ackerr <wzmmmmj@gmail.com>
2020-05-07 23:47:55 +0800

    0572(python)

---
+++ 0572.subtree_of_another_tree/solution.py
@@ -0,0 +1,23 @@
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def is_subtree(self, s: TreeNode, t: TreeNode) -> bool:
+        """ 判断本身/左子树/右子树是不是相同"""
+        if not s and not t:
+            return True
+        elif not s or not t:
+            return False
+        return self.is_same_tree(s, t) or self.is_same_tree(s.left, t) or self.is_same_tree(s.right, t)
+
+    def is_same_tree(self, m: TreeNode, n: TreeNode) -> bool:
+        """ 判断两树是不是相同 (like 100.same_tree)"""
+        if not m and not n:
+            return True
+        elif not m or not n:
+            return False
+        return m.val == n.val and self.is_same_tree(m.left, n.left) and self.is_same_tree(m.right, n.right)
