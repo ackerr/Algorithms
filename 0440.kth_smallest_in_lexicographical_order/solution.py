0d1fd31bf640647613d19b548543377a57fb3852
Ackerr <wzmmmmj@gmail.com>
2020-07-05 21:37:04 +0800

    0440(python)

---
+++ 0440.kth_smallest_in_lexicographical_order/solution.py
@@ -0,0 +1,24 @@
+class Solution:
+    def find_kth_number(self, n: int, k: int) -> int:
+        cur = 1
+        k -= 1
+
+        while k > 0:
+            step = self._step(n, cur, cur + 1)
+            # 判断是否k值在子节点中
+            if k <= step:
+                k -= step
+                cur += 1
+            else:
+                k -= 1
+                cur *= 10
+        return cur
+
+    def _step(self, n, left, right):
+        """ 两值直接的间距 """
+        step = 0
+        while left < n:
+            step = min(n + 1, right) - left
+            left *= 10
+            right *= 10
+        return step
