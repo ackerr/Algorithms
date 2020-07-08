8dc026c4c5ade3c4d8e7e674a824322d4e0d6410
Ackerr <wzmmmmj@gmail.com>
2020-07-08 08:49:12 +0800

    0105(go)

--- 0105.construct_binary_tree_from_preorder_and_inorder_traversal/solution.go
+++ 0105.construct_binary_tree_from_preorder_and_inorder_traversal/solution.go
@@ -1,1 +1,22 @@
 package leetcode0105
+
+import "github.com/Ackerr/Algorithms/utils"
+
+func buildTree(preorder []int, inorder []int) *utils.TreeNode {
+	if len(preorder) == 0 {
+		return nil
+	}
+
+	node := &utils.TreeNode{Val: preorder[0]}
+	index := 0
+	for i, val := range inorder {
+		if val == preorder[0] {
+			index = i
+			break
+		}
+	}
+	leftLength := len(inorder[:index])
+	node.Left = buildTree(preorder[1:leftLength+1], inorder[:index])
+	node.Right = buildTree(preorder[leftLength+1:], inorder[index+1:])
+	return node
+}
--- 0105.construct_binary_tree_from_preorder_and_inorder_traversal/solution_test.go
+++ 0105.construct_binary_tree_from_preorder_and_inorder_traversal/solution_test.go
@@ -1,1 +1,31 @@
 package leetcode0105
+
+import (
+	"github.com/Ackerr/Algorithms/utils"
+	"reflect"
+	"testing"
+)
+
+func TestBuildTree(t *testing.T) {
+	preorder := []int{3,9,20,15,7}
+	inorder := []int{9,3,15,20,7}
+	tree := buildTree(preorder, inorder)
+	if reflect.DeepEqual(buildInorder(tree, []int{}), inorder){
+		t.Errorf("buildTree(%+v,  %+v), result error ", preorder, inorder)
+	}
+
+}
+
+func buildInorder(node *utils.TreeNode, inorder []int) []int {
+	if node == nil{
+		return inorder
+	}
+	if node.Left != nil {
+		buildInorder(node.Left, inorder)
+	}
+	inorder = append(inorder, node.Val)
+	if node.Right != nil {
+		buildInorder(node.Right, inorder)
+	}
+	return inorder
+}
